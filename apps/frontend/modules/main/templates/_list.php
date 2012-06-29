c291c659b8cb513de52d7f281e33b6b34dcc23a8
vashyoung <michael.tille@goabroad.com>
2012-06-29 14:14:21 +0800

    update css and etc v1
    update css and etc v1

--- apps/frontend/modules/main/templates/_list.php
+++ apps/frontend/modules/main/templates/_list.php
@@ -1,8 +1,8 @@
 <ul>
 	<?php foreach($pager->getResults() as $iList): ?>
 		<li>
-			<span class="fl"><img src="/images/timer.png" border="0" alt="Duration:" class="fl mr5"> <?php echo $iList->getTime(); ?></span>
-			<h4><img src="/images/song.png" border="0" alt="Song:" class="fl mr10"> <?php echo $iList->getName(); ?> <em><small class="mr5">by</small> <?php echo $iList->getArtist(); ?></em></h4>
+			<span class="fl"><img src="/images/timer.png" border="0" alt="Duration:" class="fl mr5"> <?php echo $iList->getTime(); ?> <img src="/images/song.png" border="0" alt="Song:" class="fr"> </span>
+			<h4><?php echo $iList->getName(); ?> <em><small class="mr5">by</small> <?php echo $iList->getArtist(); ?></em></h4>
 			<a id="vote" data-id="<?php echo $iList->getId(); ?>" href="#" class="fr vote">vote this song</a>
 		</li>
    	<?php endforeach; ?>
--- apps/frontend/modules/main/templates/_playlist.php
+++ apps/frontend/modules/main/templates/_playlist.php
@@ -1,8 +1,8 @@
 <ul>
 	<?php foreach($pager->getResults() as $iList): ?>
 		<li>
-			<span class="fl"><img src="/images/timer.png" border="0" alt="Duration: <?php echo $iList->getTime(); ?>" class="fl mr5"> <?php echo $iList->getTime(); ?></span>
-			<h4><img src="/images/song.png" border="0" alt="Song: <?php echo $iList->getName(); ?>" class="fl mr10"> <?php echo $iList->getName(); ?> <em><small class="mr5">by</small> <?php echo $iList->getArtist(); ?></em></h4>
+			<span class="fl"><img src="/images/timer.png" border="0" alt="Duration: <?php echo $iList->getTime(); ?>" class="fl mr5"> <?php echo $iList->getTime(); ?> <img src="/images/song.png" border="0" alt="Song: <?php echo $iList->getName(); ?>" class="fr"></span><h4><?php echo $iList->getName(); ?> <em><small class="mr5">by</small> <?php echo $iList->getArtist(); ?></em></h4>
+
 		</li>
    	<?php endforeach; ?>
 </ul>
