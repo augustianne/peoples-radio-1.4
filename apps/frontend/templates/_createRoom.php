6ff3bddd398b1b56e3201865e9462e3085820f5a
vashyoung <michael.tille@goabroad.com>
2012-07-06 16:10:48 +0800

    update create room
    update create room

--- apps/frontend/templates/_createRoom.php
+++ apps/frontend/templates/_createRoom.php
@@ -2,8 +2,8 @@
     <h2>Create a Room!</h2>
     <form id="create-room-form" action="<?php echo url_for('create-channel') ?>" method="post" class="frm">
     	<label>Enter Room Name:</label>
-    	<input type="text" name="channel" size="83" class="text"/>
-    	<input type="submit" value="Submit" class="button create fr"/>
+    	<input type="text" name="channel" size="83" class="text clear"/>
+    	<input type="submit" value="Submit" class="button create clear fl"/>
     </form>
     <a class="close-reveal-modal">&#215;</a>
 </div>
