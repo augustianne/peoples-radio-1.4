d6028db8f38fb07883287348258e5544cfd34bba
vashyoung <michael.tille@goabroad.com>
2012-07-06 15:59:55 +0800

    fix popup
    fix popup

--- apps/frontend/templates/_createRoom.php
+++ apps/frontend/templates/_createRoom.php
@@ -1,10 +1,9 @@
 <div id="create-room-modal" class="reveal-modal">
     <h2>Create a Room!</h2>
-    <hr class="carved" />
-    <form id="create-room-form" action="<?php echo url_for('create-channel') ?>" method="post">
+    <form id="create-room-form" action="<?php echo url_for('create-channel') ?>" method="post" class="frm">
     	<label>Enter Room Name:</label>
-    	<input type="text" name="channel" size="88"/>
-    	<input type="submit" value="SUBMIT" />
+    	<input type="text" name="channel" size="83" class="text"/>
+    	<input type="submit" value="Submit" class="button create fr"/>
     </form>
     <a class="close-reveal-modal">&#215;</a>
-</div>                                          
+</div>
